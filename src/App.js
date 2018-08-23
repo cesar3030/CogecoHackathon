0737c0107f09294c44a7387b5018c5eff4847a5e
César Jeanroy <cesar.jeanroy@gmail.com>
2018-08-23 10:21:47 -0400

    Adding based components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MeetingFilter from './MeetingFilter.jsx';
+import MeetingList from './MeetingList.jsx';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.states = {
+      meetings: [],
+      rooms: [],
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div>Header</div>
+        <MeetingFilter rooms={this.states.rooms}/>
+        <MeetingList meeting={this.states.meetings}/>
       </div>
     );
   }
---
+++ src/MeetingList.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class MeetingList extends Component {
+  render(){
+    return (
+    <ul>
+      <li>Meeting 1</li>
+      <li>Meeting 2</li>
+      <li>Meeting 3</li>
+    </ul>
+  );
+  }
+}
