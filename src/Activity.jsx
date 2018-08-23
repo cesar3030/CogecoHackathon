af5f0d2b1c1e3e07ba43a7559991846d39833a38
César Jeanroy <cesar.jeanroy@gmail.com>
2018-08-23 13:29:12 -0400

    created activity component

---
+++ src/Activity.jsx
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+import './Activity.css';
+
+export default class Activity extends Component{
+  cssPercentage(){
+    return `c100 p${this.props.percentage}`
+  }
+  render() {
+    return (
+      <div className={this.cssPercentage()}>
+        <span>{this.props.percentage}%</span>
+        <div className="slice">
+          <div className="bar"></div>
+          <div className="fill"></div>
+        </div>
+      </div>
+    );
+  }
+}
